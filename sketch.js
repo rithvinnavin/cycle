f0027feb5d735b121ab1b14d758994ca3c47b66c
rithvinnavin <95083612+rithvinnavin@users.noreply.github.com>
2022-02-11 19:05:36 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -50,7 +50,7 @@ mainCyclist.scale=0.07;
 //set collider for mainCyclist
 
 //mainCyclist.setCollission("rectangle",0,0,40,40);
-mainCyclist.setCollider("rectangle",0,0,40,40);
+//mainCyclist.setCollider("rectangle",0,0,40,40);
 //mainCyclist.setCollission("rectangle",0,0,40,40,50);
 //mainCyclist.setCollider("rectangle",0,0,40,40,50);
 
@@ -157,9 +157,9 @@ function draw() {
     //   reset();
     // }
 
-     if(keyDown("UP_ARROW")) {
-       reset();
-     }
+    // if(keyDown("UP_ARROW")) {
+    //   reset();
+    // }
 }
 }
 
@@ -190,6 +190,17 @@ function redCyclists(){
         redCG.add(player3);
 }
 
+//function reset{
+//  gameState = END;
+//  gameOver.visible = false;
+//  mainCyclist.addAnimation("SahilRunning",mainRacerImg1);
+  
+//  pinkCG.destroyEach();
+//  yellowCG.destroyEach();
+//  redCG.destroyEach();
+  
+//  distance = 0;
+// }
 
 //function reset{
 //  gameState = PLAY;
@@ -214,16 +225,17 @@ function redCyclists(){
   
 //  distance = 0;
 // }
-function reset(){
- gameState = END;
- gameOver.visible = true;
- mainCyclist.addAnimation("SahilRunning",mainRacerImg1);
+
+//function reset(){
+//  gameState = END;
+//  gameOver.visible = true;
+//  mainCyclist.addAnimation("SahilRunning",mainRacerImg1);
   
- pinkCG.destroyEach();
-  yellowCG.destroyEach();
-  redCG.destroyEach();
+//  pinkCG.destroyEach();
+//  yellowCG.destroyEach();
+//  redCG.destroyEach();
   
-  distance = 50;
-}
+//  distance = 50;
+// }
 
 
